a8de44f9af73e8ad84c1e1e7864657a8d16ad8b8
renderbroken <zkennedy87@gmail.com>
2014-09-02 21:46:14 -0700

    Ramdisk_AOSP: Update
    Get rid of Rom name and make it more generic
    Signed-off-by: renderbroken <zkennedy87@gmail.com>

--- ramdisk_aosp/init.render.post_boot.sh
+++ ramdisk_aosp/init.render.post_boot.sh
@@ -3,58 +3,26 @@
 ############################
 # Power Settings
 #
-echo 4 > /sys/module/lpm_levels/enable_low_power/l2
-echo 1 > /sys/module/msm_pm/modes/cpu0/power_collapse/suspend_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu1/power_collapse/suspend_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu2/power_collapse/suspend_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu3/power_collapse/suspend_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu0/power_collapse/idle_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu1/power_collapse/idle_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu2/power_collapse/idle_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu3/power_collapse/idle_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu0/standalone_power_collapse/suspend_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu1/standalone_power_collapse/suspend_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu2/standalone_power_collapse/suspend_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu3/standalone_power_collapse/suspend_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu0/standalone_power_collapse/idle_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu1/standalone_power_collapse/idle_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu2/standalone_power_collapse/idle_enabled
-echo 1 > /sys/module/msm_pm/modes/cpu3/standalone_power_collapse/idle_enabled
 echo 1 > /sys/module/msm_pm/modes/cpu0/retention/idle_enabled
 echo 1 > /sys/module/msm_pm/modes/cpu1/retention/idle_enabled
 echo 1 > /sys/module/msm_pm/modes/cpu2/retention/idle_enabled
 echo 1 > /sys/module/msm_pm/modes/cpu3/retention/idle_enabled
-echo 1 > /sys/devices/system/cpu/cpu1/online
-echo 1 > /sys/devices/system/cpu/cpu2/online
-echo 1 > /sys/devices/system/cpu/cpu3/online
-echo 0 > /sys/kernel/sched/gentle_fair_sleepers
-chmod 664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-chown -h system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-chown -h system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-chown -h system system /sys/devices/system/cpu/cpu1/online
-chown -h system system /sys/devices/system/cpu/cpu2/online
-chown -h system system /sys/devices/system/cpu/cpu3/online
-chmod -h 664 /sys/devices/system/cpu/cpu1/online
-chmod -h 664 /sys/devices/system/cpu/cpu2/online
-chmod -h 664 /sys/devices/system/cpu/cpu3/online
-chown -h root system /sys/devices/system/cpu/mfreq
-chmod 0220 /sys/devices/system/cpu/mfreq
-echo 1 > /dev/cpuctl/apps/cpu.notify_on_migrate
-echo 1 > /sys/module/msm_thermal/core_control/enabled
 
 ############################
 # CPU-Boost Settings
 #
-#echo 0 > /sys/module/cpu_boost/parameters/boost_ms
-#echo 0 > /sys/module/cpu_boost/parameters/sync_threshold
-#echo 0 > /sys/module/cpu_boost/parameters/input_boost_freq
-#echo 0 > /sys/module/cpu_boost/parameters/input_boost_ms
-#echo 1 > /sys/module/cpu_boost/parameters/load_based_syncs
+echo 10 > /sys/module/cpu_boost/parameters/boost_ms
+echo 1728000 > /sys/module/cpu_boost/parameters/sync_threshold
+echo 1190400 > /sys/module/cpu_boost/parameters/input_boost_freq
+echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
+echo 1190400 > /sys/module/cpu_boost/parameters/plug_boost_freq
+echo 1 > /sys/module/cpu_boost/parameters/load_based_syncs
+echo 1 > /sys/module/cpu_boost/parameters/wakeup_boost
 
 ############################
 # MSM_Hotplug Settings
 #
-echo 2 > /sys/module/msm_hotplug/cpus_boosted
+echo 1 > /sys/module/msm_hotplug/cpus_boosted
 echo 500 > /sys/module/msm_hotplug/down_lock_duration
 echo 2500 > /sys/module/msm_hotplug/boost_lock_duration
 echo 200 5:100 50:50 350:200 > /sys/module/msm_hotplug/update_rates
@@ -117,7 +85,7 @@ echo 300000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
 ############################
 # Scheduler and Read Ahead
 #
-echo deadline > /sys/block/mmcblk0/queue/scheduler
+echo fiops > /sys/block/mmcblk0/queue/scheduler
 echo 1024 > /sys/block/mmcblk0/bdi/read_ahead_kb
 
 ############################
@@ -129,3 +97,5 @@ echo simple_ondemand > /sys/class/devfreq/fdb00000.qcom,kgsl-3d0/governor
 # Test Debugging!!!
 #
 echo 0 > /sys/module/subsystem_restart/parameters/enable_ramdumps
+echo 0 > /sys/kernel/sched/gentle_fair_sleepers
+echo 1 > /sys/module/msm_thermal/core_control/enabled
