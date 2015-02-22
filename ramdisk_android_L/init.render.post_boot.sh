5fcbba7ec11c3673e5fce1b26a7730b8f56b0008
Zachariah Kennedy <zkennedy87@gmail.com>
2015-02-22 13:22:03 -0500

    Render Script: Update
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>

--- ramdisk_android_L/init.render.post_boot.sh
+++ ramdisk_android_L/init.render.post_boot.sh
@@ -6,73 +6,15 @@ BB=/sbin/busybox
 # Custom Kernel Settings for Render Kernel!!
 #
 
-############################s
-# CPU-Boost Settings
-#
-echo 20 > /sys/module/cpu_boost/parameters/boost_ms
-echo 1497600 > /sys/module/cpu_boost/parameters/sync_threshold
-echo 0:1190400 1:1190400 2:1190400 3:1190400 > /sys/module/cpu_boost/parameters/input_boost_freq
-echo 60 > /sys/module/cpu_boost/parameters/input_boost_ms
-echo 1 > /sys/module/cpu_boost/parameters/load_based_syncs
-echo 1 > /sys/module/cpu_boost/parameters/wakeup_boost
-echo 1 > /sys/module/cpu_boost/parameters/hotplug_boost
-
 ############################
 # MSM_Hotplug Settings
 #
 echo 2 > /sys/module/msm_hotplug/cpus_boosted
-echo 500 > /sys/module/msm_hotplug/down_lock_duration
+echo 750 > /sys/module/msm_hotplug/down_lock_duration
 echo 2500 > /sys/module/msm_hotplug/boost_lock_duration
 echo 200 5:100 50:50 350:200 > /sys/module/msm_hotplug/update_rates
 echo 400 > /sys/module/msm_hotplug/fast_lane_load
-echo 2 > /sys/module/msm_hotplug/max_cpus_online_susp
-
-############################
-# CPUFreq and Governor Settings
-#
-
-# Ondemand
-echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo ondemand > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-echo ondemand > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-echo ondemand > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-echo 95 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
-echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
-echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
-echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-echo 10 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
-echo 75 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_multi_core
-echo 3 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential_multi_core
-echo 960000 > /sys/devices/system/cpu/cpufreq/ondemand/optimal_freq
-echo 960000 > /sys/devices/system/cpu/cpufreq/ondemand/sync_freq
-echo 85 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load
-
-# Interactive
-echo interactive > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo interactive > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-echo interactive > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-echo interactive > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-echo 20000 1400000:40000 1700000:20000 > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
-echo 90 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
-echo 1190400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
-echo 1 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
-echo 85 1500000:90 1800000:70 > /sys/devices/system/cpu/cpufreq/interactive/target_loads
-echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
-echo 30000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
-echo 100000 > /sys/devices/system/cpu/cpufreq/interactive/max_freq_hysteresis
-echo 30000 > /sys/devices/system/cpu/cpufreq/interactive/timer_slack
-
-############################
-# Set MAN-MAX Freq on boot
-#
-echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo 300000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-echo 300000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
-echo 300000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
-echo 2265600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-echo 2265600 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-echo 2265600 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
-echo 2265600 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+echo 1 > /sys/module/msm_hotplug/max_cpus_online_susp
 
 ############################
 # MSM-Limiter Freq Tuning
@@ -133,6 +75,11 @@ echo 1 > /sys/module/msm_thermal/parameters/enabled
 #
 echo 1 > /sys/module/workqueue/parameters/power_efficient
 echo 0 > /sys/module/subsystem_restart/parameters/enable_ramdumps
+
+############################
+# Activate Simple_GPU_Algorithym
+#
+echo 1 > /sys/module/simple_gpu_algorithm/parameters/simple_gpu_activate
 
 ############################
 # Synapse Support
